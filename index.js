3a427a46ad03b0867b3ef75e862e1aa0b1066f20
+ <+>
2023-02-25 00:08:37 -0600

    completed

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(names,event){
+    const newNames = [];
+    for(let i = 0; i < names.length; i++){
+        newNames[i] = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+    }
+    return newNames;
+}
+
+ function countDown(num){
+   let i = num;
+    while(i >= 0){
+        console.log(i);
+         i--;
+    }
+}
